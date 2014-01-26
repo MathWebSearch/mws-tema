b45067b8bb8f8e4cf3dbd44ded6f48fad58d2de6
Corneliu C Prodescu <cprodescu@gmail.com>
2014-01-26 01:25:09 +0100

    Add multiple paths for es data generation script.
    Allow multiple xhtml paths in scripts/generate-es-data.sh

--- scripts/generate-es-data.sh
+++ scripts/generate-es-data.sh
@@ -1,8 +1,8 @@
 #!/bin/bash -e
 
-if [ "$#" -ne 2 ]; then
+if [ "$#" -lt 2 ]; then
     cat << EOF
-Usage: $0 <json_out_dir> <xhtml_dir>
+Usage: $0 <json_out_dir> <xhtml_dir> [ <xhtml_dir> .. ]
 
 This script generates elastic search json data.
 
@@ -10,12 +10,15 @@ EOF
     exit 1
 fi
 
-ELASTIC_SEARCH_OUTPUT="$1"
-INCLUDE_PATH="$2"
-
 MWSD="$(dirname $0)/../build/mwsd"
-$MWSD                                                   \
-    --include-harvest-path $INCLUDE_PATH                \
+ELASTIC_SEARCH_OUTPUT="$1"
+
+shift # skip first argument
+for xhtml_dir in $@; do
+    INCLUDE_PATH_ARGS="$INCLUDE_PATH_ARGS --include-harvest-path $1"
+done
+
+$MWSD $INCLUDE_PATH_ARGS                                \
     --recursive                                         \
     --elastic-search-outdir $ELASTIC_SEARCH_OUTPUT      \
     --exit-after-load
